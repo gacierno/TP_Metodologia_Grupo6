9efb47a18f4a186805e198795c56ebb15be79917
GasparErrobidart <podmaster@hotmail.com>
2019-10-10 11:16:29 -0300

    router beta

--- html_cine/index.php
+++ html_cine/index.php
@@ -1,16 +1,27 @@
 <?php
- 
+
+  require_once('lib/Router.php');
+
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
-	
+
+  $router = new Router();
+  $router->get('home',function(){
+    echo "Home";
+  });
+
+  $router->find('get','home');
+
+  return "FAIL";
+
 	// require "Config/Autoload.php";
 	// require "Config/Config.php";
 
 	// use Config\Autoload as Autoload;
 	// use Config\Router 	as Router;
 	// use Config\Request 	as Request;
-		
+
 	// Autoload::start();
 
 	// session_start();
---
+++ html_cine/lib/Router.php
@@ -0,0 +1,60 @@
+<?php
+class Router {
+
+  private $map = array();
+
+  function __construct(){
+
+  }
+
+  function add($methodName,$regexp,$callback){
+    if(!isset($this->map[$methodName])){
+      $this->map[$methodName] = array();
+    }
+    array_push(
+      $this->map[$methodName], array(
+        'regexp'   => $regexp,
+        'callback' => $callback
+      )
+    );
+  }
+
+  function get($regexp, $callback){
+    $this->add('get',$regexp,$callback);
+  }
+
+  function post($regexp, $callback){
+    $this->add('post',$regexp,$callback);
+  }
+
+  function delete($regexp, $callback){
+    $this->add('delete',$regexp,$callback);
+  }
+
+  function put($regexp, $callback){
+    $this->add('put',$regexp,$callback);
+  }
+
+  function all($regexp, $callback){
+    $this->add('all',$regexp,$callback);
+  }
+
+  function find($httpMethod,$path){
+    $httpMethodArray = isset($this->map[$httpMethod]) ? $this->map[$httpMethod] : array();
+    $allMethodArray  = isset($this->map['all']) ? $this->map['all'] : array();
+    $method = array_merge(
+      $httpMethodArray,
+      $allMethodArray
+    );
+    foreach( $method as $route ){
+      $pattern = '/' . $route['regexp'] . '/';
+      if(preg_match($pattern,$path)){
+        return call_user_func($route['callback']);
+      }
+    }
+    return false;
+  }
+
+}
+
+?>
