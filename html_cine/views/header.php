b384438d237d94c987a61cfffa0dc09a9bdea94a
Franco David Marsili del Grosso <francomarsili@bytery.com>
2019-10-12 14:58:36 -0300

    CinemaList view

--- html_cine/views/header.php
+++ html_cine/views/header.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./assets/css/styles.css">
+    <link rel="stylesheet" href="public/assets/css/reset.css">
+    <link rel="stylesheet" href="public/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/assets/owlcarousel/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="public/assets/owlcarousel/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="public/assets/css/styles.css">
+    <link rel="icon" type="image/png" href="public/assets/images/favicon.png"/>
     <title>MoviePass</title>
 </head>
 <body>
@@ -12,17 +17,10 @@
 <div class="nav">
   <div class="nav__container nav-desktop">
     <div class="nav__logo--container">
-      <a routerLink="/">
-        <img src="./assets/images/black-cat.svg" >
+      <a href="/">
+        <img src="public/assets/images/black-cat.svg" >
       </a>
-    </div><div class="nav__search-box--container">
-      <input type="text">
-    </div><div class="nav__login-opt--container">
-      <ul class="nav__login--options">
-        <li><a class="nav__option--format" routerLink="/login">LOGIN</a></li>
-        <li><a class="nav__option--format" routerLink="/register">REGISTER</a></li>
-      </ul>
     </div>
-  </div>
   <div class="nav-mobile"></div>
+  </div>
 </div>
