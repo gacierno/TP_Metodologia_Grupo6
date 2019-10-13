77845df243ccc365c0e53ba8a2451fddcc0ff29a
gacierno <gacierno@gmail.com>
2019-10-13 11:46:02 -0300

    Correction For delete method on BASE DAO

--- html_cine/controller/testsController.php
+++ html_cine/controller/testsController.php
@@ -12,13 +12,116 @@
 	include_once dirname(__DIR__).'/dao/MovieDao.php';
 	include_once dirname(__DIR__).'/dao/GenreDao.php';
 
+	include_once dirname(__DIR__).'/config/settings.php';
+
+	/*
+	 * testGetList
+	 * @Params $dao : instance from a DAO class ,
+	 *				: instance from a model asociated to this DAO
+	 * @return : boolean ( true if the function pass the test )
+	 */
+	function testGetList( $dao, $obj ){
+		if( sizeof( $dao->getList() ) > 0 ){
+			return true;
+		}else{
+			$dao->add( $obj );
+			if( sizeof( $dao->getList() ) > 0 ){
+				$dao->delete( $obj );
+				return true;
+			}else{
+				return false;
+			}
+		}
+	}
+
+	/*
+	 * testGetById
+	 * @Params $dao : instance from a DAO class ,
+	 *				: instance from a model asociated to this DAO
+	 * @return : boolean ( true if the function pass the test )
+	 */
+	function testGetById( $dao, $obj ){ 
+		$dao->add( $obj );
+		if( $dao->getById( $obj->getId() ) ){
+			$dao->delete( $obj->getId(), $obj );
+			return true;
+		}else{
+			return false;
+			$dao->delete( $obj->getId(), $obj );
+		}
+	}
+
+	/*
+	 * testAdd
+	 * @Params $dao : instance from a DAO class ,
+	 *				: instance from a model asociated to this DAO
+	 * @return : boolean ( true if the function pass the test )
+	 */
+	function testAdd( $dao, $obj ){ 
+		$dao->add( $obj );
+		if( $dao->getById( $obj->getId() ) ){
+			$dao->delete( $obj->getId(), $obj );
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	/*
+	 * testUpdate
+	 * @Params $dao : instance from a DAO class ,
+	 *				: instance from a model asociated to this DAO
+	 * @return : boolean ( true if the function pass the test )
+	 */
+	function testUpdate( $dao, $obj ){ 
+		$dao->add( $obj );
+		if( $dao->getById( $obj->getId() ) ){
+			$obj->setName('passed');
+			$dao->update( $obj->getId(), $obj );
+			$test = $dao->getById( $obj->getId() );
+			if( $test->getName() == 'passed' ){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
+
+	/*
+	 * testDelete
+	 * @Params $dao : instance from a DAO class ,
+	 *				: instance from a model asociated to this DAO
+	 * @return : boolean ( true if the function pass the test )
+	 */
+	function testDelete( $dao, $obj ){ 
+		$dao->add( $obj );
+		if( $dao->getById( $obj->getId() ) ){
+			$dao->delete( $obj->getId(), $obj );
+			if( !$dao->getById( $obj->getId() ) ){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
 
 
 
+
+	/*
+	 *	hardcoded cinema to perform tests
+	 */
+	$cinemaForTest = new model\Cinema( "Test Cinema","Test Address",-1,-1,-1 );
+	/*
+	 * Instance CinemaDao for testing
+	 */
 	$cinemaDao = new dao\CinemaDao();
 
-	$cinemas = $cinemaDao->getList();
-	
+
 
 
 
