3b13c06f26677f1feef00fd94385568f876f9bd7
Franco David Marsili del Grosso <francomarsili@bytery.com>
2019-10-14 00:30:03 -0300

    Eliminate duplicated item

--- html_cine/views/movieItems.php
+++ html_cine/views/movieItems.php
@@ -10,15 +10,4 @@
                     </div>
                 </div>
             </div>
-            <div class="item movielist__card--container" style="background-image:url('<?php echo($movie->getImage()); ?>');background-position:center;background-size:cover;">
-                <div class="movielist__card--overlay">
-                    <div class="moviecard__info--container">
-                        <div class="moviecard__name--container"><h2><?php echo($movie->getName()); ?></h2></div>
-                        <hr>
-                        <div class="moviecard__duration--container"><h4><?php echo($movie->getDuration().' minutos'); ?></h4></div>
-                        <div class="moviecard__language--container"><h4><?php echo($movie->getLanguage()); ?></h4></div>
-                        <div class="moviecard__genre--container"><h4><?php foreach($movie->getGenres() as $genre) : echo($genre); endforeach; ?></h4></div>
-                    </div>
-                </div>
-            </div>
 <?php endforeach; ?>
