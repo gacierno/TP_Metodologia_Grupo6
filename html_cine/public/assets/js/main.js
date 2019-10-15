4462ad50f593880fb95bf3f705baec056c84c449
Franco David Marsili del Grosso <francomarsili@bytery.com>
2019-10-15 08:17:48 -0300

    remove goto slider

--- html_cine/public/assets/js/main.js
+++ html_cine/public/assets/js/main.js
@@ -113,7 +113,6 @@ function buildCarousel(selector,destroy){
             }
         }
     };
-    bindSliderItems();
 
     return($(selector).owlCarousel( carousel_settings ));
 
@@ -151,10 +150,14 @@ function checkArrowsVisibility(){
 
 }
 
-function bindSliderItems(){
-    $(moviesSlider).find('.item').each(function(index){
-        $(this).on('click',function(){
-            $(moviesSlider).trigger('to.owl.carousel',index);
-        })
-    })
-}
+// function bindSliderItems(){
+//     var length =  $(moviesSlider).find('.owl-item').length;
+//     var cardnum;
+//     $(moviesSlider).find('.owl-item').each(function(index){
+//         cardnum = (length/2) - (index+1);
+//         $(this).attr('slider-num',cardnum);
+//         $(this).on('click',function(){
+//             $(moviesSlider).trigger('to.owl.carousel',cardnum);
+//         })
+//     })
+// }
