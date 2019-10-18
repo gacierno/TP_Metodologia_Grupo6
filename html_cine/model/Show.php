381ddfa4329e514dcdd47ce1c5dd93b5fbb908c8
gacierno <gacierno@gmail.com>
2019-10-18 11:39:34 -0300

    Show model

--- html_cine/model/Show.php
+++ html_cine/model/Show.php
@@ -8,11 +8,15 @@ class Show
 	
 	private $day;
 	private $time;
+    private $movie;
+    private $cinema;
 
-	function __construct( $day = '1980-01-01', $time = '00:00' )
+	function __construct( $day = '1980-01-01', $time = '00:00', $movie = null, $cinema = null )
 	{
 		$this->setTime($time);
 		$this->setDay($day);
+        $this->setMovie($movie);
+        $this->setCinema($cinema);
 	}
 
 
@@ -47,6 +51,38 @@ class Show
     public function setTime($time)
     {
         $this->time = $time;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMovie()
+    {
+        return $this->movie;
+    }
+
+    /**
+     * @param mixed $movie
+     */
+    public function setMovie($movie)
+    {
+        $this->movie = $movie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCinema()
+    {
+        return $this->cinema;
+    }
+
+    /**
+     * @param mixed $cinema
+     */
+    public function setCinema($cinema)
+    {
+        $this->cinema = $cinema;
     }
 }
  ?>
