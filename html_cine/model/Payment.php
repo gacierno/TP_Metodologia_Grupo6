c3732d1835062321f5b45ca216d5756e9e973b40
gacierno <gacierno@gmail.com>
2019-10-22 09:17:52 -0300

    Payment MODEL

---
+++ html_cine/model/Payment.php
@@ -0,0 +1,99 @@
+<?php 
+/**
+  * 
+  */
+ class Payment
+ {
+
+ 	private $id;
+ 	private $authCode;
+ 	private $date;
+ 	private $amount;
+ 	private $count;		//	:int id account
+ 	
+ 	function __construct(argument)
+ 	{
+ 		# code...
+ 	}
+ 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthCode()
+    {
+        return $this->authCode;
+    }
+
+    /**
+     * @param mixed $authCode
+     */
+    public function setAuthCode($authCode)
+    {
+        $this->authCode = $authCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param mixed $amount
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCount()
+    {
+        return $this->count;
+    }
+
+    /**
+     * @param mixed $count
+     */
+    public function setCount($count)
+    {
+        $this->count = $count;
+    }
+} 
+ ?>
