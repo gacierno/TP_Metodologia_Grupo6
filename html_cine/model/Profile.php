16ac18dbb658ffd92fe1e1b314f40f49443439f8
gacierno <gacierno@gmail.com>
2019-10-22 10:08:49 -0300

    Profile MODEL

---
+++ html_cine/model/Profile.php
@@ -0,0 +1,89 @@
+<?php 
+namespace model;
+/**
+ * 
+ */
+class Profile
+{
+	
+	private $id;
+	private $nombre;
+	private $apellido;
+	private $dni;
+
+
+	function __construct( $nomnre, $apellido, $dni, $id )
+	{
+		$this->setApellido($apellido);
+		$this->setDni($dni);
+		$this->setId($id);
+		$this->setNombre($nombre);
+	}
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    /**
+     * @param mixed $apellido
+     */
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDni()
+    {
+        return $this->dni;
+    }
+
+    /**
+     * @param mixed $dni
+     */
+    public function setDni($dni)
+    {
+        $this->dni = $dni;
+    }
+}
+ ?>
