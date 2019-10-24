d56d92cbde5545dd11a987bb8150e12a67111b45
GasparErrobidart <podmaster@hotmail.com>
2019-10-24 12:03:34 -0300

    Router, middlewares, authentication , login

---
+++ html_cine/controller/AuthenticationController.php
@@ -0,0 +1,23 @@
+<?php namespace Controller;
+
+use Controller\BaseController as BaseController;
+use Response\RedirectResponse as RedirectResponse;
+use Request\Request as Request;
+
+class AuthenticationController extends BaseController{
+
+  function authenticate(){
+    // $req = new Request();
+    // session_start();
+    // extract($_SESSION);
+    // if(!isset($user) && $req->path() != '/login'){
+    //   $response = new RedirectResponse("/login");
+    //   return $response->send();
+    // }
+  }
+
+  function login(){
+    echo "<html><head></head><body><h1>LOGIN</h1></body></html>";
+  }
+
+}
