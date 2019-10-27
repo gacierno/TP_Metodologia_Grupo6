df1aaa09d501e8cef1d32b6a3715273e77d911e6
Franco David Marsili del Grosso <francomarsili@bytery.com>
2019-10-27 19:13:47 -0300

    WIP

---
+++ html_cine/views/movieDetail.php
@@ -0,0 +1,16 @@
+<?php include_once('header.php'); ?>
+
+
+<div class="main-container container-fluid">
+    <div class="row movie-detail__container">
+        <div class="col-sm-12 col-md-4 movie-detail__banner--container">
+            <img src="/public/assets/images/try.jpg">
+        </div>
+        <div class="col-sm-12 col-md-8 movie-detail__desc--container">
+            <h1>TRYOUT</h1>
+            
+        </div>
+    </div>
+</div>
+
+<?php include_once('footer.php'); ?>
