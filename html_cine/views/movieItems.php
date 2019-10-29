b39a73aa5959b33e38facbf32bcdb2264369c22e
Franco David Marsili del Grosso <francomarsili@bytery.com>
2019-10-29 12:51:51 -0300

    Detail update

--- html_cine/views/movieItems.php
+++ html_cine/views/movieItems.php
@@ -1,5 +1,6 @@
 <?php foreach($movies as $movie) : ?>
             <div class="item movielist__card--container" style="background-image:url('<?php echo('https://image.tmdb.org/t/p/original'.$movie->getImage()); ?>');background-position:center;background-size:cover;">
+                <a href="/pelicula/detalle?id=<?php echo($movie->getId()); ?>">
                 <div class="movielist__card--overlay">
                     <div class="moviecard__info--container">
                         <div class="moviecard__name--container"><h2><?php echo($movie->getName()); ?></h2></div>
@@ -25,5 +26,6 @@
                         }  ?></h4></div>
                     </div>
                 </div>
+                </a>
             </div>
 <?php endforeach; ?>
