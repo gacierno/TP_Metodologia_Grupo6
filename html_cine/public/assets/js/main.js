1c97e806f3f91c04039d130733192f0fc4115489
Franco David Marsili del Grosso <francomarsili@bytery.com>
2019-11-20 21:46:00 -0300

    js update date

--- html_cine/public/assets/js/main.js
+++ html_cine/public/assets/js/main.js
@@ -148,7 +148,9 @@ $(document).ready(function(){
             event.preventDefault();
             event.stopPropagation();
             var genre = $('#moviefilter__multiple-select--genre').val();
-            if(genre.indexOf("all") >=0){
+            console.log(genre);
+
+            if(genre.indexOf("all") >=0 || genre.length == 0){
                 genre = null;
             }
             var date = $('#moviefilter__select--date').val();
