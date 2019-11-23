080db196ebf23f981f63039bfb90867d6a61702b
GasparErrobidart <podmaster@hotmail.com>
2019-11-22 21:12:46 -0300

    Implement MP

---
+++ html_cine/controller/SalesController.php
@@ -0,0 +1,76 @@
+<?php namespace Controller;
+
+use Controller\BaseController as BaseController;
+
+use DAO\ShowDao             	as ShowDao;
+use Model\Show                as Show;
+
+// use DAO\PurchaseDao        as PurchaseDao;
+use Model\Purchase            as Purchase;
+
+class SalesController extends BaseController{
+
+
+
+  function __construct(){
+    parent::__construct();
+  }
+
+  function checkout(){
+    $d_show       = new ShowDao();
+    $show         = $d_show->getById($this->params->show_id);
+    $purchaseDate = new \DateTime();
+    $quantity     = $this->params->ticket_quantity;
+    $ticketValue  = $show->getCinemaRoom()->getTicketValue();
+    $amount       = $ticketValue * $quantity;
+    $purchase = new Purchase(
+      array(
+        'purchase_ticket_qty' => $quantity,
+        'purchase_discount' => 0,
+        'purchase_date' => $purchaseDate->format('Y-m-d'),
+        'purchase_amount' => $amount,
+        'purchase_tickets' => null,
+        'purchase_user' => $this->session->user,
+        'purchase_payment' => null
+      )
+    );
+
+
+    // Agrega credenciales
+    \MercadoPago\SDK::setAccessToken(MP_ACCESS_TOKEN);
+
+    // Crea un objeto de preferencia
+    $preference = new \MercadoPago\Preference();
+
+    // Crea un ítem en la preferencia
+    $item = new \MercadoPago\Item();
+    $item->title =
+      $show->getMovie()->getName() .
+      ' - ' . $show->getDay() . ', ' . $show->show_time . ' ' .
+      $show->getCinemaRoom()->getCinema()->getName() . ' ' . $show->getCinemaRoom()->getName();
+    $item->quantity = $purchase->getTicketQty();
+    $item->unit_price = $ticketValue;
+    $preference->items = array($item);
+    $preference->save();
+
+
+    $payment_button = <<<EOD
+    <form action="/procesar-pago" method="POST">
+      <script
+       src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+       data-preference-id="$preference->id">
+      </script>
+    </form>
+EOD;
+
+
+    $this->render('checkout',
+      array(
+        'purchase'        => $purchase,
+        'show'            => $show,
+        'payment_button'  => $payment_button
+      )
+    );
+  }
+
+}
--- html_cine/views/checkout.php
+++ html_cine/views/checkout.php
@@ -6,7 +6,6 @@ $show que tiene los datos a mostrar de la funcion -->
 
 <?php include_once('partials/customMessage.php'); ?>
 
-
 <div class="main-container container-fluid purchase__list--container">
 <?php if(isset($purchase)) : ?>
     <h1>Checkout</h1>
@@ -14,17 +13,14 @@ $show que tiene los datos a mostrar de la funcion -->
     <div class="purchase__outer--container">
         <div class="purchase__item--container">
             <p class="">Fecha de la funcion</p>
-            <span><?php echo($show->getDate()); ?></span>
+            <span><?php echo($show->getDay()); ?></span>
         </div>
 
-        <div class="purchase__item--container">
-            <p class="">Nombre de la funcion</p>
-            <span><?php echo($show->getName()); ?></span>
-        </div>
+
 
         <div class="purchase__item--container">
             <p class="">Cantidad de Tickets</p>
-            <span><?php echo($purchase->getTicket_qty()); ?></span>
+            <span><?php echo($purchase->getTicketQty()); ?></span>
         </div>
 
         <div class="purchase__item--container">
@@ -38,18 +34,14 @@ $show que tiene los datos a mostrar de la funcion -->
         <div class="purchase__item--container">
             <p class="">Descuento Aplicado</p>
             <span><?php echo($purchase->getDiscount()); ?></span>
-        </div>  
+        </div>
 
         <div class="purchase__item--container">
             <p class="">Monto Total</p>
             <span><?php echo($purchase->getAmount()); ?></span>
-        </div>  
+        </div>
 
-        <form method="POST" action="/test-payment">
-        <input style="display:none" type="text" name="show_id" value="<?php echo($show->getId()); ?>">
-        <input style="display:none" type="number" name="ticket_qty" value="<?php echo($purchase->getTicket_qty()); ?>">
-        <button type="submit" class="btn btn-info">Comprar</button>
-        </form>
+        <?= $payment_button; ?>
 
     </div>
 
